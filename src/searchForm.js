ccbc48944fdaf6aa5fd57cb0cc860791828cdf73
afolasope <folasopeadebanjo@gmail.com>
2024-12-24 07:41:34 +0100

    update credentials

--- src/searchForm.js
+++ src/searchForm.js
@@ -1,4 +1,4 @@
-import { getElement, setLocalStorage, getStorageItem } from './utils.js';
+import { getElement, KEY_WEATHER, setLocalStorage } from './utils.js';
 
 const searchFormCon = getElement('.city-input');
 const searchCardCon = getElement('.search-items');
@@ -6,65 +6,70 @@ const searchInput = getElement('.city-search-input');
 const errorHandle = getElement('.error-handle');
 
 searchInput.addEventListener('keyup', async function () {
-  errorHandle.classList.remove('hidden');
-  errorHandle.textContent = 'please wait';
-  try {
-    const value = searchInput.value;
-    searchCardCon.style.display = 'none';
-    const fetchData = await fetch(
-      `https://api.weatherstack.com/autocomplete?access_key=ecb932b2f749d26f72b9c7d10c604550&query=${value}`
-    );
-    const response = await fetchData.json();
-    console.log(response);
-    searchCardCon.style.display = 'block';
+    errorHandle.classList.remove('hidden');
+    errorHandle.textContent = 'please wait';
+    console.log('first|||||||||||||||||||||||');
+    try {
+        const value = searchInput.value;
+        searchCardCon.style.display = 'none';
+        const fetchData = await fetch(`http://api.weatherstack.com/current?access_key=${KEY_WEATHER}&query=${value}`);
+        const response = await fetchData.json();
+        searchCardCon.style.display = 'block';
 
-    if (response.request) {
-      errorHandle.classList.remove('hidden');
-      errorHandle.classList.add('hidden');
-      const { results } = response;
-      const info = results.map(function (result) {
-        const items = [result.name, result.country];
-        return items;
-      });
-      searchCardCon.classList.remove('hidden');
-      const searchDisplay = info.map(function (item) {
-        return `
+        if (response.request) {
+            errorHandle.classList.remove('hidden');
+            errorHandle.classList.add('hidden');
+            const { results } = response;
+
+            console.log(results);
+            console.log('first||||||||||||||||||||||||||||||||||||||');
+
+            // const info = results.map(function (result) {
+            //     const items = [result.name, result.country];
+            //     return items;
+            // });
+
+            const info = [results.location.name, results.location.country];
+            console.log({ info });
+
+            console.log(results);
+
+            searchCardCon.classList.remove('hidden');
+
+            const searchDisplay = info.map(function (item) {
+                return `
            <a href="./citydetails.html" class="search-item" data-id="${item[0]}, ${item[1]}">${item[0]}, ${item[1]}</a>
             `;
-      });
-      searchCardCon.innerHTML = searchDisplay.join('');
-    }
-    if (
-      response.error ||
-      response.error.code === 602 ||
-      response.error.code === 601
-    ) {
-      errorHandle.textContent = 'no result found';
-      searchCardCon.style.display = 'none';
-      setTimeout(() => {
-        errorHandle.classList.add('hidden');
-      }, 1000);
-    }
-  } catch (error) {}
+            });
+            searchCardCon.innerHTML = searchDisplay.join('');
+        }
+        if (response.error || response.error.code === 602 || response.error.code === 601) {
+            errorHandle.textContent = 'no result found';
+            searchCardCon.style.display = 'none';
+            setTimeout(() => {
+                errorHandle.classList.add('hidden');
+            }, 1000);
+        }
+    } catch (error) {}
 });
 
 searchInput.addEventListener('focusout', function (e) {
-  if (e.relatedTarget && e.relatedTarget.classList.contains('search-item')) {
-    return;
-  }
-  searchCardCon.style.display = 'none';
-  searchInput.value = '';
+    if (e.relatedTarget && e.relatedTarget.classList.contains('search-item')) {
+        return;
+    }
+    searchCardCon.style.display = 'none';
+    searchInput.value = '';
 });
 
 const searchItems = getElement('.search-items');
 searchFormCon.addEventListener('click', async function (e) {
-  try {
-    const searchItem = e.target.classList.contains('search-item');
-    if (searchItem) {
-      const id = e.target.dataset.id;
-      setLocalStorage('cityId', id);
+    try {
+        const searchItem = e.target.classList.contains('search-item');
+        if (searchItem) {
+            const id = e.target.dataset.id;
+            setLocalStorage('cityId', id);
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 });
--- src/utils.js
+++ src/utils.js
@@ -1,49 +1,40 @@
 const getElement = (selection) => {
-  const element = document.querySelector(selection);
-  if (element) return element;
-  throw new Error(
-    `Please check "${selection}" selector, no such element exist`
-  );
+    const element = document.querySelector(selection);
+    if (element) return element;
+    throw new Error(`Please check "${selection}" selector, no such element exist`);
 };
 
 const topCities = [
-  'Beijing',
-  'Buenos Aires',
-  'Cairo',
-  'Chongqing',
-  'Delhi',
-  'Dhaka',
-  'Karachi',
-  'Mexico City',
-  'Mumbai',
-  'New York',
-  'Osaka',
-  'Seoul',
-  'Sao Paulo',
-  'Shangai',
-  'Tokyo',
+    'Beijing',
+    'Buenos Aires',
+    'Cairo',
+    'Chongqing',
+    'Delhi',
+    'Dhaka',
+    'Karachi',
+    'Mexico City',
+    'Mumbai',
+    'New York',
+    'Osaka',
+    'Seoul',
+    'Sao Paulo',
+    'Shangai',
+    'Tokyo',
 ];
 
 const setLocalStorage = function (name, item) {
-  localStorage.setItem(name, JSON.stringify(item));
+    localStorage.setItem(name, JSON.stringify(item));
 };
 const getStorageItem = (item) => {
-  let storageItem = localStorage.getItem(item);
-  if (!storageItem) {
-    return null;
-  }
+    let storageItem = localStorage.getItem(item);
+    if (!storageItem) {
+        return null;
+    }
 
-  return JSON.parse(localStorage.getItem(item));
+    return JSON.parse(localStorage.getItem(item));
 };
 
-const KEY_WEATHER = 'ecb932b2f749d26f72b9c7d10c604550';
+const KEY_WEATHER = '6fb2350322db6ae123184c88adf5c5ed';
 const GOOGLE_MAP = 'AIzaSyCa7ux5MjN9vMKX3ETuyvP5Al9glu3sFqY';
 
-export {
-  getElement,
-  topCities,
-  setLocalStorage,
-  getStorageItem,
-  KEY_WEATHER,
-  GOOGLE_MAP,
-};
+export { getElement, getStorageItem, GOOGLE_MAP, KEY_WEATHER, setLocalStorage, topCities };
